2e6a8a44b237470f65401a8fd52179f9eea15467
yangjunchao888 <625977644@qq.com>
2022-02-25 17:36:38 +0800

    viper engine and usage

---
+++ sample/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/JuncYoung/goConf"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+const ProjectName = "Sample"
+
+func main() {
+	goConf.AddSearchPath("E:\\qnzs\\aiqc\\src\\goConf/sample")
+	if err := goConf.InitViperConfig(viper.GetViper(), ProjectName, "c"); err != nil {
+		panic(err)
+	}
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		log.Printf("config file changed: %s", e.Name) // update configuration
+	})
+
+	value := viper.GetString("test.myKey")
+	log.Printf("value: %s\n", value)
+}
